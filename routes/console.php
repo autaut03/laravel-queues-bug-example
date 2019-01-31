85d715bb736f372124e8beb5431ba90c4b9e8bcd
autaut03 <autaut03@gmail.com>
2019-01-31 18:08:02 +0200

    Bug implementation

--- routes/console.php
+++ routes/console.php
@@ -13,6 +13,8 @@ use Illuminate\Foundation\Inspiring;
 |
 */
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->describe('Display an inspiring quote');
+Artisan::command('seed-job', function () {
+    \App\Jobs\MainJob::dispatch();
+
+    $this->info('Done');
+})->describe('Seed 1 job of type MainJob');
